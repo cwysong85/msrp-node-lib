a198a5de86d31e39a0e28f40f0ab0e3ee8db9bee
Corey Wysong <cwysong85@gmail.com>
2016-10-10 10:32:01 -0400

    added "done" event to server start

--- src/Server.js
+++ src/Server.js
@@ -3,45 +3,52 @@ var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
 module.exports = function(MsrpSdk) {
-  var msrpTracingEnabled = false;
-  var config = MsrpSdk.Config;
+    var msrpTracingEnabled = false;
+    var config = MsrpSdk.Config;
 
-  /**
-   * MSRP server
-   * @param  {object} config Holds the MSRP Server configuation
-   * @return {object}        The MSRP Server object
-   */
-  var Server = function() {
-    var server = this;
-    server.config = config;
-    msrpTracingEnabled = config.traceMsrp;
+    /**
+     * MSRP server
+     * @param  {object} config Holds the MSRP Server configuation
+     * @return {object}        The MSRP Server object
+     */
+    var Server = function() {
+        var server = this;
+        server.config = config;
+        msrpTracingEnabled = config.traceMsrp;
 
-    this.sessions = {};
+        this.sessions = {};
 
-    this.server = net.createServer(function(socket){
-      // TODO: INBOUND -  Remove next line
-      console.warn('CALLING SOCKET HANDLER FROM SERVER');
+        this.server = net.createServer(function(socket) {
+            // TODO: INBOUND -  Remove next line
+            console.warn('CALLING SOCKET HANDLER FROM SERVER');
 
-      // TODO: INBOUND - Review this
-      socket = new MsrpSdk.SocketHandler(socket);
-    });
-  };
+            // TODO: INBOUND - Review this
+            socket = new MsrpSdk.SocketHandler(socket);
+        });
+    };
 
-  // TODO: Is Server emitting something now?
-  util.inherits(Server, EventEmitter);
+    // TODO: Is Server emitting something now?
+    util.inherits(Server, EventEmitter);
 
-  Server.prototype.start = function() {
-    var server = this;
-    this.server.listen(config.port, config.host, function() {
-      var serv = server.server.address();
-      console.info('MSRP TCP server listening on ' + serv.address + ':' + serv.port);
-      if (msrpTracingEnabled) {
-        console.info('MSRP tracing enabled');
-      }
-    }).on('error', function(error) {
-      console.warn(error);
-    });
-  };
+    Server.prototype.start = function(done) {
+        var server = this;
+        this.server.listen(config.port, config.host, function() {
+            var serv = server.server.address();
+            console.info('MSRP TCP server listening on ' + serv.address + ':' + serv.port);
+            if (msrpTracingEnabled) {
+                console.info('MSRP tracing enabled');
+            }
 
-  MsrpSdk.Server = Server;
+            if (done) {
+                done();
+            }
+        }).on('error', function(error) {
+            console.warn(error);
+            if (done) {
+                done(error);
+            }
+        });
+    };
+
+    MsrpSdk.Server = Server;
 };
