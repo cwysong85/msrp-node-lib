701e8e1232367d2ecc01ee6034cd2215c57ae60f
Corey Wysong <cwysong@indigital.net>
2018-06-02 09:24:38 -0400

    TCP connection is opening faster than the signalling library was sending its 200 ok

--- src/Session.js
+++ src/Session.js
@@ -4,219 +4,223 @@ var EventEmitter = require('events').EventEmitter;
 
 module.exports = function(MsrpSdk) {
 
-    var Session = function() {
-        this.sid = MsrpSdk.Util.newSID();
-        this.localEndpoint = null;
-        this.remoteEndpoints = [];
-        this.localSdp = null;
-        this.remoteSdp = null;
-        this.socket = null;
-        this.reinvite = false;
-        this.setup = MsrpSdk.Config.setup;
-        this.setHasNotRan = true;
-        this.getHasNotRan = true;
-    };
+  var Session = function() {
+    this.sid = MsrpSdk.Util.newSID();
+    this.localEndpoint = null;
+    this.remoteEndpoints = [];
+    this.localSdp = null;
+    this.remoteSdp = null;
+    this.socket = null;
+    this.reinvite = false;
+    this.setup = MsrpSdk.Config.setup;
+    this.setHasNotRan = true;
+    this.getHasNotRan = true;
+  };
 
-    util.inherits(Session, EventEmitter); // Sessions emit events in SocketHandler
+  util.inherits(Session, EventEmitter); // Sessions emit events in SocketHandler
 
-    Session.prototype.sendMessage = function(body, cb) {
+  Session.prototype.sendMessage = function(body, cb) {
 
-        var canSend = false;
-        for (var i = this.remoteSdp.attributes.acceptTypes.length - 1; i >= 0; i--) {
-            if (this.remoteSdp.attributes.acceptTypes[i] === "text/plain" || this.remoteSdp.attributes.acceptTypes[i] === "*") {
-                canSend = true;
-            }
-        }
+    var canSend = false;
+    for (var i = this.remoteSdp.attributes.acceptTypes.length - 1; i >= 0; i--) {
+      if (this.remoteSdp.attributes.acceptTypes[i] === "text/plain" || this.remoteSdp.attributes.acceptTypes[i] === "*") {
+        canSend = true;
+      }
+    }
 
-        if (canSend) {
-            this.socket.emit('send', {
-                body: body,
-                contentType: "text/plain"
-            }, {
-                toPath: this.remoteEndpoints,
-                localUri: this.localEndpoint.uri
-            }, cb);
+    if (canSend) {
+      this.socket.emit('send', {
+        body: body,
+        contentType: "text/plain"
+      }, {
+        toPath: this.remoteEndpoints,
+        localUri: this.localEndpoint.uri
+      }, cb);
+    } else {
+      MsrpSdk.Logger.warn('[MSRP Session] Cannot send message due to incompatible content types exchanged in SDP');
+      return;
+    }
+  };
+
+  Session.prototype.getRemoteEndpoint = function(remoteEndpointUri) {
+    var session = this;
+    for (var i = 0; i < session.remoteEndpoints.length; i++) {
+      if (session.remoteEndpoints[i] === remoteEndpointUri) {
+        return session.remoteEndpoints[i];
+      }
+    }
+  };
+
+  Session.prototype.addRemoteEndpoint = function(remoteEndpointUri) {
+    this.remoteEndpoints.push(remoteEndpointUri);
+  };
+
+  Session.prototype.getDescription = function(onSuccess, onFailure, mediaHint) {
+    var session = this;
+
+    var sdp = new MsrpSdk.Sdp.Session();
+    if (!sdp) {
+      return;
+    }
+
+    // Origin
+    sdp.origin.id = MsrpSdk.Util.dateToNtpTime(new Date());
+    sdp.origin.version = sdp.origin.id;
+    sdp.origin.address = MsrpSdk.Config.host;
+
+    // Session-name
+    sdp.sessionName = MsrpSdk.Config.sessionName;
+
+    // Configuration port. Do we do TLS???
+    sdp.media.push("message " + MsrpSdk.Config.port + " TCP/MSRP *");
+
+    // Connection: If connection is present, update it
+    if (sdp.connection) {
+      sdp.connection.address = MsrpSdk.Config.host;
+    }
+
+    // Accept-types
+    sdp.addAttribute('accept-types', MsrpSdk.Config.acceptTypes);
+
+    // Path
+    var path = 'msrp://' + MsrpSdk.Config.host + ':' + MsrpSdk.Config.port + '/' + this.sid + ';tcp';
+    sdp.addAttribute('path', path);
+
+    // Setup line
+    if (session.remoteSdp) {
+      // has attributes and setup line
+      if (session.remoteSdp.attributes.setup) {
+        if (session.remoteSdp.attributes.setup[0] === 'active') {
+          sdp.addAttribute('setup', 'passive');
+        } else if (session.remoteSdp.attributes.setup[0] === 'passive' || session.remoteSdp.attributes.setup[0] === 'actpass') {
+          sdp.addAttribute('setup', 'active');
         } else {
-            MsrpSdk.Logger.warn('[MSRP Session] Cannot send message due to incompatible content types exchanged in SDP');
-            return;
+          return onFailure('Invalid a=setup value');
         }
-    };
+      } else {
+        sdp.addAttribute('setup', 'active');
+      }
+    } else {
+      sdp.addAttribute('setup', session.setup);
+    }
 
-    Session.prototype.getRemoteEndpoint = function(remoteEndpointUri) {
-        var session = this;
-        for (var i = 0; i < session.remoteEndpoints.length; i++) {
-            if (session.remoteEndpoints[i] === remoteEndpointUri) {
-                return session.remoteEndpoints[i];
-            }
-        }
-    };
+    session.localSdp = sdp;
+    session.localEndpoint = new MsrpSdk.URI(path);
 
-    Session.prototype.addRemoteEndpoint = function(remoteEndpointUri) {
-        this.remoteEndpoints.push(remoteEndpointUri);
-    };
+    session.getHasNotRan = false;
 
-    Session.prototype.getDescription = function(onSuccess, onFailure, mediaHint) {
-        var session = this;
+    if (session.reinvite) {
+      if (session.remoteSdp.attributes.setup[0] !== 'active') {
+        sdp.addAttribute('setup', 'active');
+        session.startConnection(function() {
+          // MsrpSdk.Logger.info('REINVITING');
+          session.emit('reinvite', session);
+          session.reinvite = false;
+        });
+      } else {
+        session.emit('reinvite', session);
+        session.reinvite = false;
+      }
+    } else {
+      session.startConnection();
+    }
 
-        var sdp = new MsrpSdk.Sdp.Session();
-        if (!sdp) {
-            return;
-        }
+    onSuccess(sdp.toString());
+  };
 
-        // Origin
-        sdp.origin.id = MsrpSdk.Util.dateToNtpTime(new Date());
-        sdp.origin.version = sdp.origin.id;
-        sdp.origin.address = MsrpSdk.Config.host;
+  Session.prototype.setDescription = function(description, onSuccess, onFailure) {
+    var session = this;
 
-        // Session-name
-        sdp.sessionName = MsrpSdk.Config.sessionName;
+    // pass to SDP parser, create a room, get SDP answer
+    var sdp = new MsrpSdk.Sdp.Session(description);
 
-        // Configuration port. Do we do TLS???
-        sdp.media.push("message " + MsrpSdk.Config.port + " TCP/MSRP *");
+    // check to see if remote is active or passive
+    for (var i = 0; i < sdp.media.length; i++) {
+      // If client doesn't have the setup line its most likely wrong
+      // RFC 4975 states that if a MSRP client initiates a connection it should therefore be active
+      // unless it cannot be active, therefore set the "setup" attribute to "passive"
+      //
+      // draft-pd-dispatch-msrp-websocket-08 doesn't have the active, passive, actpass attribute defined
+      // it might be a good idea to have the relay add this to SDP or build it into the MSRP SDP
 
-        // Connection: If connection is present, update it
-        if (sdp.connection) {
-            sdp.connection.address = MsrpSdk.Config.host;
-        }
-
-        // Accept-types
-        sdp.addAttribute('accept-types', MsrpSdk.Config.acceptTypes);
-
-        // Path
-        var path = 'msrp://' + MsrpSdk.Config.host + ':' + MsrpSdk.Config.port + '/' + this.sid + ';tcp';
-        sdp.addAttribute('path', path);
-
-        // Setup line
-        if (session.remoteSdp) {
-            // has attributes and setup line
-            if (session.remoteSdp.attributes.setup) {
-                if (session.remoteSdp.attributes.setup[0] === 'active') {
-                    sdp.addAttribute('setup', 'passive');
-                } else if (session.remoteSdp.attributes.setup[0] === 'passive' || session.remoteSdp.attributes.setup[0] === 'actpass') {
-                    sdp.addAttribute('setup', 'active');
-                } else {
-                    return onFailure('Invalid a=setup value');
-                }
-            } else {
-                sdp.addAttribute('setup', 'active');
-            }
+      if (sdp.media[i].attributes) {
+        if (sdp.media[i].attributes.setup) {
+          sdp.attributes.setup = sdp.media[i].attributes.setup;
         } else {
-            sdp.addAttribute('setup', session.setup);
+          sdp.attributes.setup = ['passive'];
         }
+      } else {
+        sdp.attributes.setup = ['passive'];
+      }
 
-        session.localSdp = sdp;
-        session.localEndpoint = new MsrpSdk.URI(path);
+      if (sdp.media[i].attributes.path) {
+        sdp.attributes.path = sdp.media[i].attributes.path[0].split(' ');
+        session.remoteEndpoints = sdp.attributes.path;
+      }
 
-        session.getHasNotRan = false;
+      if (sdp.media[i].attributes['accept-types']) {
+        sdp.attributes.acceptTypes = sdp.media[i].attributes['accept-types'][0].split(' ');
+      }
+    }
 
-        if (session.reinvite) {
-            if (session.remoteSdp.attributes.setup[0] !== 'active') {
-                sdp.addAttribute('setup', 'active');
-                session.startConnection(function() {
-                    // MsrpSdk.Logger.info('REINVITING');
-                    session.emit('reinvite', session);
-                    session.reinvite = false;
-                });
-            } else {
-                session.emit('reinvite', session);
-                session.reinvite = false;
-            }
-        } else {
-            session.startConnection();
-        }
+    if (session.remoteSdp) {
+      session.reinvite = true;
+    }
 
-        onSuccess(sdp.toString());
-    };
+    session.remoteSdp = sdp;
+    session.setHasNotRan = false;
+    session.startConnection();
+    onSuccess();
+  };
 
-    Session.prototype.setDescription = function(description, onSuccess, onFailure) {
-        var session = this;
+  Session.prototype.end = function() {
+    if (this.socket) {
+      this.socket.end();
+    }
+  };
 
-        // pass to SDP parser, create a room, get SDP answer
-        var sdp = new MsrpSdk.Sdp.Session(description);
+  Session.prototype.startConnection = function(cb) {
+    var session = this;
 
-        // check to see if remote is active or passive
-        for (var i = 0; i < sdp.media.length; i++) {
-            // If client doesn't have the setup line its most likely wrong
-            // RFC 4975 states that if a MSRP client initiates a connection it should therefore be active
-            // unless it cannot be active, therefore set the "setup" attribute to "passive"
-            //
-            // draft-pd-dispatch-msrp-websocket-08 doesn't have the active, passive, actpass attribute defined
-            // it might be a good idea to have the relay add this to SDP or build it into the MSRP SDP
+    if (session.getHasNotRan || session.setHasNotRan) {
+      return;
+    }
 
-            if (sdp.media[i].attributes) {
-                if (sdp.media[i].attributes.setup) {
-                    sdp.attributes.setup = sdp.media[i].attributes.setup;
-                } else {
-                    sdp.attributes.setup = ['passive'];
-                }
-            } else {
-                sdp.attributes.setup = ['passive'];
-            }
+    if (!session.remoteSdp || !session.localSdp) {
+      return;
+    }
 
-            if (sdp.media[i].attributes.path) {
-                sdp.attributes.path = sdp.media[i].attributes.path[0].split(' ');
-                session.remoteEndpoints = sdp.attributes.path;
-            }
+    var createConnection = function() {
+      if (session.localSdp.attributes.setup[0] === 'active') {
+        // We are active, lets create a socket to them
+        var remoteEndpointUri = new MsrpSdk.URI(session.remoteEndpoints[0]);
+        var socket = new net.Socket();
+        session.socket = new MsrpSdk.SocketHandler(socket);
 
-            if (sdp.media[i].attributes['accept-types']) {
-                sdp.attributes.acceptTypes = sdp.media[i].attributes['accept-types'][0].split(' ');
-            }
-        }
+        socket.connect(remoteEndpointUri.port, remoteEndpointUri.authority, function() {
 
-        if (session.remoteSdp) {
-            session.reinvite = true;
-        }
+          // TODO: (LVM) Our bodiless message is different to the one the client send us.
+          // Send bodiless MSRP message
+          var request = new MsrpSdk.Message.OutgoingRequest({
+            toPath: session.remoteEndpoints,
+            localUri: session.localEndpoint.uri
+          }, 'SEND');
 
-        session.remoteSdp = sdp;
-        session.setHasNotRan = false;
-        session.startConnection();
-        onSuccess();
-    };
-
-    Session.prototype.end = function() {
-        if (this.socket) {
-            this.socket.end();
-        }
-    };
-
-    Session.prototype.startConnection = function(cb) {
-        var session = this;
-
-        if (session.getHasNotRan || session.setHasNotRan) {
-            return;
-        }
-
-        if (!session.remoteSdp) {
-            return;
-        }
-
-        if (session.remoteSdp.attributes.setup[0] !== 'active') {
-            // We are active, lets create a socket to them
-            var remoteEndpointUri = new MsrpSdk.URI(session.remoteEndpoints[0]);
-            var socket = new net.Socket();
-            session.socket = new MsrpSdk.SocketHandler(socket);
-
-            socket.connect(remoteEndpointUri.port, remoteEndpointUri.authority, function() {
-
-                // TODO: (LVM) Our bodiless message is different to the one the client send us.
-                // Send bodiless MSRP message
-                var request = new MsrpSdk.Message.OutgoingRequest({
-                    toPath: session.remoteEndpoints,
-                    localUri: session.localEndpoint.uri
-                }, 'SEND');
-
-                try {
-                    socket.write(request.encode(), function() {
-                        if (cb) {
-                            cb()
-                        };
-                    });
-                } catch (e) {
-                    MsrpSdk.Logger.error(e);
-                }
+          try {
+            socket.write(request.encode(), function() {
+              if (cb) {
+                cb()
+              };
             });
-        }
+          } catch (e) {
+            MsrpSdk.Logger.error(e);
+          }
+        });
+      }
     };
 
-    MsrpSdk.Session = Session;
+    setTimeout(createConnection, 300);
+  };
+
+  MsrpSdk.Session = Session;
 };
