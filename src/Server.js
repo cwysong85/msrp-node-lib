615a602d0094090613c9408e7e2b095c224ffd31
Luis Villaseñor Muñoz <luis.villasenor.munoz@indigital.net>
2019-02-06 15:22:39 +0100

    - Server.js alignment

--- src/Server.js
+++ src/Server.js
@@ -3,47 +3,47 @@ var util = require('util');
 var EventEmitter = require('events').EventEmitter;
 
 module.exports = function(MsrpSdk) {
-    var msrpTracingEnabled = false;
-    var config = MsrpSdk.Config;
+  var msrpTracingEnabled = false;
+  var config = MsrpSdk.Config;
 
-    /**
-     * MSRP server
-     * @param  {object} config Holds the MSRP Server configuation
-     * @return {object}        The MSRP Server object
-     */
-    var Server = function() {
-        var server = this;
-        server.config = config;
-        msrpTracingEnabled = config.traceMsrp;
+  /**
+   * MSRP server
+   * @param  {object} config Holds the MSRP Server configuation
+   * @return {object}        The MSRP Server object
+   */
+  var Server = function() {
+    var server = this;
+    server.config = config;
+    msrpTracingEnabled = config.traceMsrp;
 
-        this.sessions = {};
+    this.sessions = {};
 
-        this.server = net.createServer(function(socket) {
-            socket = new MsrpSdk.SocketHandler(socket);
-        });
-    };
+    this.server = net.createServer(function(socket) {
+      socket = new MsrpSdk.SocketHandler(socket);
+    });
+  };
 
-    util.inherits(Server, EventEmitter);
+  util.inherits(Server, EventEmitter);
 
-    Server.prototype.start = function(callback) {
-        var server = this;
-        this.server.listen(config.port, config.host, function() {
-            var serv = server.server.address();
-            MsrpSdk.Logger.info('MSRP TCP server listening on ' + serv.address + ':' + serv.port);
-            if (msrpTracingEnabled) {
-                MsrpSdk.Logger.info('MSRP tracing enabled');
-            }
+  Server.prototype.start = function(callback) {
+    var server = this;
+    this.server.listen(config.port, config.host, function() {
+      var serv = server.server.address();
+      MsrpSdk.Logger.info('MSRP TCP server listening on ' + serv.address + ':' + serv.port);
+      if (msrpTracingEnabled) {
+        MsrpSdk.Logger.info('MSRP tracing enabled');
+      }
 
-            if (callback) {
-                callback();
-            }
-        }).on('error', function(error) {
-            MsrpSdk.Logger.warn(error);
-            if (callback) {
-                callback(error);
-            }
-        });
-    };
+      if (callback) {
+        callback();
+      }
+    }).on('error', function(error) {
+      MsrpSdk.Logger.warn(error);
+      if (callback) {
+        callback(error);
+      }
+    });
+  };
 
-    MsrpSdk.Server = Server;
+  MsrpSdk.Server = Server;
 };
